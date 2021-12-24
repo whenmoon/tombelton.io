6de3db9168b8eb650b8fff90133bfadffd1cdaf2
Tom Belton <tombelton@hotmail.co.uk>
2021-12-24 23:28:42 +0000

    feat: added ant design

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require("next-compose-plugins");
+const withAntdLess = require("next-plugin-antd-less");
+
+const pluginAntdLess = withAntdLess({
+	modifyVars: {
+		hack: `true;@import "${require.resolve(
+			"./styles/vendor-theme/less/theme.less"
+		)}";`,
+	},
+	lessVarsFilePath: "./styles/vendor-theme/less/variables.less",
+});
+
+module.exports = withPlugins([[pluginAntdLess]], {
+	images: {
+		formats: ["image/avif", "image/webp"],
+	},
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+require('../styles/vendor-theme/less/theme.less');
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
 export default MyApp
