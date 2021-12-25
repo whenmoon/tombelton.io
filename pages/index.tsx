0a3b8a1d0d4526b0fc0559ebc47f1982b23374c7
Tom Belton <tombelton@hotmail.co.uk>
2021-12-25 16:41:45 +0000

    feat: added ant design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,80 +1,87 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
+import React from 'react';
 import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react';
-import { Menu } from 'antd';
-import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu, Button } from 'antd';
+import {
+	VideoCameraTwoTone,
+	GithubFilled,
+	LinkedinFilled,
+	BgColorsOutlined,
+	BulbTwoTone,
+	ExperimentTwoTone,
+	MenuUnfoldOutlined,
+	MenuFoldOutlined,
+	ThunderboltTwoTone
+} from '@ant-design/icons';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const { SubMenu } = Menu;
 
 const Home: NextPage = () => {
-	const [selectedNavItem, setSelectedNavItem] = useState({ current: '' });
 	const [loading, setLoading] = useState(true);
+	const [darkTheme, setDarkTheme] = useState(false)
+	const [menuCollapsed, setMenuCollapsed] = useState(false)
 	useEffect(() => {
-
+		setTimeout(() => {
+			toast.dark("Welcome! Use the menu on the left to view stuff! :)")
+		}, 1000);
 	}, [])
 
-	const handleClick = (e: any) => {
-		console.log('click ', e);
-		setSelectedNavItem({ current: e.key });
-	};
-
 	return (
-		<div className={styles.container}>
+		<div className={darkTheme ? styles.containerDark : styles.container}>
 			<main className={styles.main}>
-				<Menu onClick={handleClick} selectedKeys={[selectedNavItem.current]} mode="horizontal">
-					<Menu.Item key="mail" icon={<MailOutlined />}>
-						Me
-					</Menu.Item>
-					<Menu.Item key="app" icon={<AppstoreOutlined />}>
-						Projects
-					</Menu.Item>
-					<Menu.Item key="app" icon={<AppstoreOutlined />}>
-						GitHub
-					</Menu.Item>
-					<Menu.Item key="app" icon={<AppstoreOutlined />}>
-						LinkedIn
-					</Menu.Item>
-				</Menu>
-				<a href="https://nextjs.org/docs" >
-					<h1 className={styles.title}>
-						Projects
-					</h1>
-				</a>
-				{/*<div className={styles.grid}>
-					<a href="https://nextjs.org/docs" className={styles.card}>
-						<h2>About &rarr;</h2>
-						<p>What I do..</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h2>Projects &rarr;</h2>
-						<p>What I've built..</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className={styles.card}
+				<ToastContainer
+					position="top-right"
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					pauseOnHover
+				/>
+				<div style={{ width: 256, position: "absolute", top: '2%', left: '2%', right: 0 }}>
+					<Button
+						type="primary"
+						onClick={() => setMenuCollapsed(prevTheme => (!prevTheme))}
+						style={{ marginBottom: 16 }}
+						icon={menuCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+					/>
+					<Menu
+						defaultSelectedKeys={['0']}
+						defaultOpenKeys={['sub1']}
+						mode="inline"
+						theme={darkTheme ? "dark" : "light"}
+						inlineCollapsed={menuCollapsed}
 					>
-						<h2>GitHub &rarr;</h2>
-						<p>Codes 'n' stuff..</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h2>LinkedIn &rarr;</h2>
-						<p>
-							Where I've worked..
-						</p>
-					</a>
-				</div>*/}
+						<SubMenu key="sub1" icon={<ThunderboltTwoTone />} title="Projects">
+							<Menu.Item key="5" icon={<VideoCameraTwoTone />}>Fleeting</Menu.Item>
+							<Menu.Item key="6" icon={<ExperimentTwoTone />}>Dumpstr</Menu.Item>
+						</SubMenu>
+						<Menu.Item key="1" icon={<BulbTwoTone />}>
+							About Me
+						</Menu.Item>
+						<Menu.Item key="2" icon={<GithubFilled />}>
+							GitHub
+						</Menu.Item>
+						<Menu.Item key="3" icon={<LinkedinFilled />}>
+							LinkedIn
+						</Menu.Item>
+					</Menu>
+				</div>
+				<Button
+					type="primary"
+					size="large"
+					onClick={() => setDarkTheme(prevTheme => (!prevTheme))}
+					icon={<BgColorsOutlined />}
+					style={{ position: "absolute", bottom: '2%', left: '2%', right: 0 }}
+				/>
 			</main>
 			{/*<footer className={styles.footer}>
 				<p>
 					Built with NextJS and TypeScript by Tom Belton
 				</p>
 			</footer>*/}
-		</div>
+		</div >
 	)
 }
 
